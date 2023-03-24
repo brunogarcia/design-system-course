db601dc7333885a2041d45fa56f04684c7df0a0a
Bruno Garcia <bruno.garcia.echegaray@gmail.com>
2023-03-24 16:21:43 +0100

    feat [react] use tailwindcss

---
+++ packages/react/tailwind.config.cjs
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+
+// eslint-disable-next-line no-undef
+module.exports = {
+  // eslint-disable-next-line no-undef
+  presets: [require('@brunogarcia/foundation-ds/lib/tailwindPreset.config')],
+  content: ['./src/**/*.{html,js,jsx,tsx,ts}'],
+  plugins: [],
+};
